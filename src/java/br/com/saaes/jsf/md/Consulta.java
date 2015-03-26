a0141129b70dd739f37e837575eb40438046f954
Jean <jeanfernandesbh@github.com>
2015-03-25 23:10:08 -0300

    Atualização 25/03/2015

---
+++ src/java/br/com/saaes/jsf/md/Consulta.java
@@ -0,0 +1,37 @@
+package br.com.saaes.jsf.md;
+
+import br.com.saesdb.util.JPAUtil;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author f4679646
+ */
+@ManagedBean
+public class Consulta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    public static final String USUARIO = "usuario";
+
+    private EntityManager em;
+    private String teste = "Consultas";
+
+    @PostConstruct
+    public void init() {
+        em = JPAUtil.getEm();
+    }
+
+    public String getTeste() {
+        return teste;
+    }
+
+    public void setTeste(String teste) {
+        this.teste = teste;
+    }
+
+}
