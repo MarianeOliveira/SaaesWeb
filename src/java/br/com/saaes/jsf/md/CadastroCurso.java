5d161eec8d5d565961c281634e9dc838cbe4ccf0
Jean <jeanfernandesbh@github.com>
2015-03-30 20:42:58 -0300

    30/03/2015

---
+++ src/java/br/com/saaes/jsf/md/CadastroCurso.java
@@ -0,0 +1,15 @@
+package br.com.saaes.jsf.md;
+
+/**
+ *
+ * @author tefi
+ */
+public class CadastroCurso extends Operacional{
+
+    public CadastroCurso() {
+        super.init();
+    }
+    
+    
+    
+}
--- test/br/com/teste/db/DBTeste.java
+++ test/br/com/teste/db/DBTeste.java
@@ -2,6 +2,8 @@
 package br.com.teste.db;
 
 import br.com.saaes.modelo.T900Usuario;
+import br.com.saaes.modelo.T902Cursos;
+import br.com.saesdb.dao.DAO;
 import br.com.saesdb.util.JPAUtil;
 import javax.persistence.EntityManager;
 import org.junit.Test;
@@ -15,11 +17,17 @@ public class DBTeste {
     private EntityManager em = JPAUtil.getEm();
     
     @Test
-    public void testeConexaoMysql() {
+    public void testeConexaoMysql() throws Exception {
         
         T900Usuario user = em.find(T900Usuario.class,6L);
         
         assertTrue(user != null ? true : false);
+        
+        T902Cursos t901 = new T902Cursos();
+        
+        DAO.save(t901, null, em, Boolean.FALSE);
+
+        assertTrue(t901 != null ? true : false);
     }
     
 }
